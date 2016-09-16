f5a4160cd61945b80375b47927cc846596bb3a70
KaushikNeelichetty <kaushikneelichetty6132@gmail.com>
2016-09-17 01:56:14 +0530

    Update DHT22_ubidots.py

--- DHT22_ubidots.py
+++ DHT22_ubidots.py
@@ -10,13 +10,13 @@ pin = 4
 
 # Create an ApiClient object
 
-api = ApiClient(token='ljGqF2u0ZCwHADAukMoE7shORQeD4O')
+api = ApiClient(token='xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 # Get a Ubidots Variable
 
 try:
-    variable1 = api.get_variable("57db89de7625425bd03a2f5e")
-    variable2 = api.get_variable("57db8a007625425d9746e931")
+    variable1 = api.get_variable("xxxxxxxxxxxxxxxxxxxxxxxx")
+    variable2 = api.get_variable("xxxxxxxxxxxxxxxxxxxxxxxx")
 
 except ValueError:
     print"It is not possible to obtain the variable"
@@ -24,11 +24,9 @@ except ValueError:
 while(1):
     try:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-        value1 = random.randint(1, 100)
         variable1.save_value({'value': humidity})
         variable2.save_value({'value': temperature})
-
-        print "Value sent"
+	print "Value sent"
         time.sleep(120)
     except ValueError:
         print "Value not sent"
